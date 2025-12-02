19611b320297e8d233de3444f73a079837b99050
frhel <freyr.helga@gmail.com>
2025-12-02 14:45:02 +0100

    Optimize Day 2 solution

--- src/Solutions/Day2.php
+++ src/Solutions/Day2.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace frhel\adventofcode2025php\Solutions;
 
 use frhel\adventofcode2025php\Tools\Prenta;
-use frhel\adventofcode2025php\Tools\Utils;
+use frhel\adventofcode2025php\Tools\Timer;
 
 class Range {
     public $lower;
@@ -34,58 +34,68 @@ class Day2 extends Day
         $data = $this->parse_input($this->load_data($this->day, $this->ex)); $this->data = $data;
 
         [$part1, $part2] = $this->solve_both($data, $part1, $part2);
-
         return [$part1, $part2];
     }
 
     protected function solve_both($data, &$part1, &$part2) {
         $id = "";
         foreach ($data as $range) {
+            // Walk through all IDs in current range
             for ($i = $range->lower; $i <= $range->upper; $i++) {
+                // Convert ID to string for easier processing
                 $id = (string)$i;
-                [$has_sequence, $is_half] = $this->contains_sequence($id);
-                if ($has_sequence) {
-                    if ($is_half && strlen($id) % 2 === 0) {
-                        $part1 += $i;
-                    }
-                    $part2 += $i;
+                $result = $this->contains_sequence($id, strlen($id));
+
+                // 0 = no sequence
+                // 1 = sequence of the full length (part 1)
+                // 2 = smaller sequence (part 2)
+                if ($result === 0) {
+                    continue;
+                } else if ($result === 1) {
+                    $part1 += $i;
                 }
+                $part2 += $i;
             }
         }
         return [$part1, $part2];
     }
 
-    protected function contains_sequence($id) {
-        $half = (int) floor(strlen($id) / 2);
+    protected function contains_sequence($id, $idLen) {
+        // Save size of half the id as a starting point
+        // to reduce the search space. If we find a sequence
+        // of the largest possible size, we can stop immediately.
+        $half = (int)($idLen / 2);
         $size = $half;
+
         while ($size > 0) {
-            $valid = true;
-            if (strlen($id) % $size !== 0) {
+            // If id length is not divisible by size, skip this size
+            if ($idLen % $size !== 0) {
                 $size--;
                 continue;
             }
+
+            // Grab the base substring and compare it to the rest
             $base = substr($id, 0, $size);
-            for ($i = ((int)strlen($id) / $size); $i > 0; $i--) {
-                if (substr($id, ($i - 1) * $size, $size) !== $base) {
-                    $valid = false;
-                    break;
+
+            // Iterate over the number of expected sequences. We can skip the first
+            // one as it's the base we're comparing to.
+            for ($chunk_count = ($idLen / $size); $chunk_count > 1; $chunk_count--) {
+                // Compare to the substring at the current position.
+                // Continue the outer while loop if we find a mismatch.
+                if (substr($id, ($chunk_count - 1) * $size, $size) !== $base) {
+                    $size--;
+                    continue 2;
                 }
             }
-            if ($valid) {
-                if ($size === $half) {
-                    return [true, true];
-                }
-                return [true, false];
+
+            // If we reach here, we found a matching sequence and can return true.
+            if ($size === $half) {
+                return 1;
             }
-            $size--;
+            return 2;
         }
+        return 0;
     }
-
-    protected function is_half_sequence($id) {
-        $half = (int) (strlen($id) / 2);
-        return substr($id, 0, $half) === substr($id, -$half);
-    }
-
 
     /**
      * Parses the input data into a usable format
